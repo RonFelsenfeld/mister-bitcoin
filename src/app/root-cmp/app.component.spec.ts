63a6f066b8c2997aecadcb84480e688535a68cbf
Ron Felsenfeld <ronfelsenfeld@gmail.com>
2024-05-02 14:46:31 +0300

    init repo

--- src/app/root-cmp/app.component.spec.ts
+++ src/app/root-cmp/app.component.spec.ts
@@ -23,7 +23,6 @@ describe('AppComponent', () => {
   it(`should have as title 'mister-bitcoin'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('mister-bitcoin');
   });
 
   it('should render title', () => {
