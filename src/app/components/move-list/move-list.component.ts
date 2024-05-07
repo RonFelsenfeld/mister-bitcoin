e897d73f00960594683dedd348f2f5de2af2a122
Ron Felsenfeld <ronfelsenfeld@gmail.com>
2024-05-07 09:36:44 +0300

    Refactor move list logic

--- src/app/components/move-list/move-list.component.ts
+++ src/app/components/move-list/move-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Move } from '../../models/move.model';
 import { Contact } from '../../models/contact.model';
 
@@ -7,8 +7,15 @@ import { Contact } from '../../models/contact.model';
   templateUrl: './move-list.component.html',
   styleUrl: './move-list.component.scss'
 })
-export class MoveList {
+export class MoveList implements OnInit {
   @Input() moves!: Move[]
   @Input() title!: string
   @Input() contact: Contact | null = null
+
+  ngOnInit(): void {
+    // If in homepage -> show last 3 moves
+    if (!this.contact) {
+      this.moves = this.moves.reverse().slice(0, 3)
+    }
+  }
 }
